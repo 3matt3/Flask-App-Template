6c01ef7a6ef5fdaa0781d5754bc59f1bd3759284
3matt3 <mmatt333@protonmail.com>
2024-09-05 22:19:47 +1000

    basic landing page

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template
+
+
+#
+#   To start test server. Be in main directory in console. In console python -m flask --app main run
+#
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/admin")
+def admin():
+    return render_template('admin.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+
